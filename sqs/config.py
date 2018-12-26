40bbe9a906d6c8cb5dd86834575e451063bbd1b7
Tonye Jack <tonye@coherentpath.com>
2018-12-26 14:26:20 -0500

    Removed api keys.

--- sqs/config.py
+++ sqs/config.py
@@ -3,8 +3,8 @@ import boto3
 
 class BaseSQSConfig(object):
     QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/141793718810/my-data-ingestor-dev'
-    ACCESS_KEY = 'AKIAJBYYYKOF4QVOPJOA'
-    SECRET_KEY = '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'
+    ACCESS_KEY = ''
+    SECRET_KEY = ''
     DEFAULT_REGION = 'us-east-1'
 
     def __init__(self):

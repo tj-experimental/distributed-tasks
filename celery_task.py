7165079b70636ef20a93301e288e6f85b7b5eca2
Tonye Jack <tonye@coherentpath.com>
2021-01-18 15:40:13 -0500

    Updated to test connectionerror.

--- celery_task.py
+++ celery_task.py
@@ -13,25 +13,25 @@ logging.basicConfig(level=logging.DEBUG)
 
 log = logging.getLogger(__name__)
 
-app = Celery('celery_task', backend='redis://localhost:6379/0', broker='redis://localhost:6379/0')
+app = Celery('celery_task', broker='redis://localhost:6379/0')
 
 
 # My locations.
 LATITUDE = 43.765204
 LONGITUDE = -79.502520
 
-app.conf.beat_schedule = {
-    # Executes at sunset in Melbourne
-    'add-at-toronto-sunset': {
-        'task': 'celery_tasks.add',
-        'schedule': solar('sunrise', LATITUDE, LONGITUDE),
-        'args': (16, 16),
-    },
-}
+# app.conf.beat_schedule = {
+#     # Executes at sunset in Melbourne
+#     'add-at-toronto-sunset': {
+#         'task': 'celery_tasks.add',
+#         'schedule': solar('sunrise', LATITUDE, LONGITUDE),
+#         'args': (16, 16),
+#     },
+# }
 
 
-@app.task(name='celery_tasks.add')
-def add(x, y):
+@app.task(name='celery_tasks.test_post')
+def test_post(x, y):
     total = x + y
     print(f'{x} + {y} = {total}')
     time.sleep(10)
@@ -63,32 +63,32 @@ def backoff(attempts):
 #         raise self.retry(exc=exc, countdown=backoff(self.request.retries))
 
 
-@periodic_task(bind=True, run_every=(crontab(minute='*/1')), ignore_result=True)
-def send_mail_queue(self):
-    try:
-        messages_sent = "example.email"
-        print(f"Task: {self.name}")
-        print(f"Total email message successfully sent {messages_sent}.")
-    finally:
-        print("release resources")
-
-key = '151361115230283ACB4F556778CBE87789100212620510281'
-
-@periodic_task(bind=True, run_every=timedelta(seconds=5), name='celery_task.send_mail_from_queue')
-def send_mail_from_queue(self):
-    REDIS_CLIENT = redis.Redis()
-    timeout = 60 * 5  # lock expires in 5 minutes
-    have_lock = False
-    my_lock = REDIS_CLIENT.lock(key, timeout=timeout)
-    try:
-        have_lock = my_lock.acquire(blocking=False)
-        if have_lock:
-            message = 'example.email'
-            print(f'{self.request.hostname}: Email Sent successfully, [{message}]')
-            time.sleep(10)
-    finally:
-        print('release resources')
-        if have_lock:
-            my_lock.release()
+# @periodic_task(bind=True, run_every=(crontab(minute='*/1')), ignore_result=True)
+# def send_mail_queue(self):
+#     try:
+#         messages_sent = "example.email"
+#         print(f"Task: {self.name}")
+#         print(f"Total email message successfully sent {messages_sent}.")
+#     finally:
+#         print("release resources")
+#
+# key = '151361115230283ACB4F556778CBE87789100212620510281'
+#
+# @periodic_task(bind=True, run_every=timedelta(seconds=5), name='celery_task.send_mail_from_queue')
+# def send_mail_from_queue(self):
+#     REDIS_CLIENT = redis.Redis()
+#     timeout = 60 * 5  # lock expires in 5 minutes
+#     have_lock = False
+#     my_lock = REDIS_CLIENT.lock(key, timeout=timeout)
+#     try:
+#         have_lock = my_lock.acquire(blocking=False)
+#         if have_lock:
+#             message = 'example.email'
+#             print(f'{self.request.hostname}: Email Sent successfully, [{message}]')
+#             time.sleep(10)
+#     finally:
+#         print('release resources')
+#         if have_lock:
+#             my_lock.release()
 
 
